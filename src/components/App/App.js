a367adb5a5413ce8b242a741310267eca055312f
Владимир Шевченко <tiigrus@gmail.com>
2025-01-28 22:39:26 +0200

    feat: integrate CharInfo component and add decor element to App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,9 @@
 import AppHeader from '../AppHeader/AppHeader';
+import CharInfo from '../CharInfo/CharInfo';
 import CharList from '../CharList/CharList';
 import RandomChar from '../RandomChar/RandomChar';
 import './App.scss';
+import vision from "../../assets/images/vision.png";
 
 function App() {
   return (
@@ -11,7 +13,9 @@ function App() {
         <RandomChar />
         <div className='char__content'>
           <CharList />
+          <CharInfo />
         </div>
+        <img src={vision} alt='vision' className='bg-decoration' aria-hidden="true" />
       </main>
     </div>
   );
