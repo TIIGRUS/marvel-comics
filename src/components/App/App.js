1b46d2eee50727d2dd332fe10668e021885d1fb2
Владимир Шевченко <tiigrus@gmail.com>
2025-01-28 22:20:53 +0200

    feat: add RandomChar component with styling and integrate into App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,5 @@
 import AppHeader from '../AppHeader/AppHeader';
+import RandomChar from '../RandomChar/RandomChar';
 import './App.scss';
 
 function App() {
@@ -6,7 +7,7 @@ function App() {
     <div className="app">
       <AppHeader />
       <main>
-
+        <RandomChar />
       </main>
     </div>
   );
