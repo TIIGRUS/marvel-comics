afade8dd30951042b2f467327668a3835a8aff9a
Владимир Шевченко <tiigrus@gmail.com>
2025-02-06 22:32:47 +0200

    feat: implement character loading and display in CharList component

--- src/components/CharList/CharList.js
+++ src/components/CharList/CharList.js
@@ -1,52 +1,62 @@
+import { Component } from "react";
+import MarvelService from "../../services/MarvelService";
+import Spinner from "../Spinner/Spinner";
 import "./CharList.scss"
-import abyss from '../../assets/images/abyss.jpg';
+// import abyss from '../../assets/images/abyss.jpg';
 
-const CharList = () => {
-    return (
-        <div className="char__list">
-            <ul className="char__grid">
-                <li className="char__item">
-                    <img src={abyss} alt="abyss" />
-                    <div className="char__name">Abyss</div>
+class CharList extends Component {
+    state = {
+        arrayChars: [],
+        isLoading: true
+    }
+
+    marvelService = new MarvelService();
+
+    onCharsLoaded = (arrayChars) => {
+        this.setState({
+            arrayChars,
+            isLoading: false
+        })
+    }
+
+    getAllChars = () => {
+        this.marvelService.getAllCharacters()
+            .then(this.onCharsLoaded)
+            .catch(e => console.error("Error fetching characters", e));
+    }
+
+    componentDidMount() {
+        this.getAllChars();
+    }
+
+    render() {
+        const { arrayChars, isLoading } = this.state;
+        const items = arrayChars.map(({ name, thumbnail }) => {
+            return (
+                <li key={name} className="char__item">
+                    <img src={thumbnail} alt={name} />
+                    <div className="char__name">{name}</div>
                 </li>
-                <li className="char__item char__item_selected">
-                    <img src={abyss} alt="abyss" />
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss" />
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss" />
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss" />
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss" />
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss" />
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss" />
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss" />
-                    <div className="char__name">Abyss</div>
-                </li>
-            </ul>
-            <button className="button button__main button__long">
-                <div className="inner">load more</div>
-            </button>
-        </div>
-    )
+            )
+        });
+        const spinner = isLoading ? <li><Spinner /></li> : null;
+
+        return (
+            <div className="char__list">
+                <ul className="char__grid">
+                    {isLoading ? spinner : items}
+
+                    {/* <li className="char__item">
+                        <img src={abyss} alt="abyss" />
+                        <div className="char__name">Abyss</div>
+                    </li> */}
+                </ul>
+                <button className="button button__main button__long">
+                    <div className="inner">load more</div>
+                </button>
+            </div>
+        )
+    }
 }
 
 export default CharList;
