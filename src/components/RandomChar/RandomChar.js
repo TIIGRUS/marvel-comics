6dc9186d616a12c66ed8c3fd3d3cb91d810aa8a9
Владимир Шевченко <tiigrus@gmail.com>
2025-02-11 23:45:30 +0200

    feat: refactor loading state handling in RandomChar component

--- src/components/RandomChar/RandomChar.js
+++ src/components/RandomChar/RandomChar.js
@@ -31,12 +31,16 @@ class RandomChar extends Component {
         });
     }
 
+    onCharLoading = () => {
+        this.setState({
+            loading: true
+        })
+    }
+
     updateChar = () => {
         const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
 
-        this.setState({
-            loading: true
-        });
+        this.onCharLoading();
 
         this.marvelService.getCharacter(id)
             .then(this.onCharLoaded).catch(e => {
