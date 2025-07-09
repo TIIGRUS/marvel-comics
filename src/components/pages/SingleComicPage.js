ba71098b85335924131af7c19d18629b9c3e92d9
Владимир Шевченко <tiigrus@gmail.com>
2025-07-09 13:53:37 +0300

    fix: enhance error message in SingleComicPage with detailed content and navigation link

--- src/components/pages/SingleComicPage.js
+++ src/components/pages/SingleComicPage.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { redirect, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import useMarvelService from "../../services/MarvelService";
 import SingleComic from "../SingleComic/SingleComic";
 import Spinner from "../Spinner/Spinner";
@@ -19,7 +19,10 @@ const SingleComicPage = () => {
     }
 
     const spinner = isLoading ? <Spinner /> : null;
-    const errorMessage = error ? <ErrorMessage /> : null;
+    const errorMessage = error ? <ErrorMessage textLink="Go back to comics list" pathLink="/comics">
+        <h1>Comic not found</h1>
+        <p>We couldn't find the comic you were looking for.</p>
+    </ErrorMessage> : null;
     const content = !(isLoading || error || !comic) ? <SingleComic comic={comic} /> : null;
 
     return (
