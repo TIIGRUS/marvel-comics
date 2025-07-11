5dfddd1af017d6b5329fc7a0c918500d69d58f29
Владимир Шевченко <tiigrus@gmail.com>
2025-07-11 21:54:18 +0300

    refactor: reorganize imports and wrap Routes in Suspense for better loading handling

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,16 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Header from '../Header/Header';
-import { MainPage, ComicsPage, NoMatch, SingleComicPage } from '../pages';
+// import { MainPage, ComicsPage, NoMatch, SingleComicPage } from '../pages';
+import Spinner from "../Spinner/Spinner";
 import vision from '../../assets/images/vision.png';
 import './App.scss';
+
+const MainPage = lazy(() => import('../pages/MainPage'));
+const ComicsPage = lazy(() => import('../pages/ComicsPage'));
+const NoMatch = lazy(() => import('../pages/NoMatch'));
+const SingleComicPage = lazy(() => import('../pages/SingleComicPage'));
 
 const App = () => {
   return (
@@ -11,12 +18,14 @@ const App = () => {
       <div className="app">
         <Header />
         <main className='app__container'>
-          <Routes>
-            <Route path="/" element={<MainPage />}></Route>
-            <Route path="/comics" element={<ComicsPage />} />
-            <Route path="/comics/:comicId" element={<SingleComicPage />} />
-            <Route path="*" element={<NoMatch />}></Route>
-          </Routes>
+          <Suspense fallback={<Spinner />}>
+            <Routes>
+              <Route path="/" element={<MainPage />}></Route>
+              <Route path="/comics" element={<ComicsPage />} />
+              <Route path="/comics/:comicId" element={<SingleComicPage />} />
+              <Route path="*" element={<NoMatch />}></Route>
+            </Routes>
+          </Suspense>
         </main>
         <img src={vision} alt='vision' className='app__decor' aria-hidden="true" />
       </div>
