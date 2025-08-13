f18f9bc43c6e12c2e6d0f2dc6a5ec5fcce339a9b
Владимир Шевченко <tiigrus@gmail.com>
2025-08-13 13:47:50 +0300

    feat: create SingleItemLayout component for comic and characher details

--- src/components/SingleComic/SingleComic.js
+++ src/components/SingleComic/SingleComic.js
@@ -1,21 +1,19 @@
-import { Link } from 'react-router-dom';
-import './SingleComic.scss';
+import SingleItemLayout from '../SingleItemLayout/SingleItemLayout';
 
 const SingleComic = ({ comic }) => {
     const { title, description, pageCount, language, price, thumbnail } = comic || {};
 
     return (
-        <div className="single-comic">
-            <img src={thumbnail} alt={title} className="single-comic__img" />
-            <div className="single-comic__info">
-                <h2 className="single-comic__name">{title}</h2>
-                <p className="single-comic__descr">{description}</p>
-                <p className="single-comic__descr">{pageCount} pages</p>
-                <p className="single-comic__descr">Language: {language}</p>
-                <div className="single-comic__price">{price}$</div>
-            </div>
-            <Link to="/comics" className="single-comic__back">Back to all</Link>
-        </div>
+        <SingleItemLayout
+            title={title}
+            description={description}
+            thumbnail={thumbnail}
+            backLink="/comics"
+        >
+            <p className="single-item__descr">{pageCount} pages</p>
+            <p className="single-item__descr">Language: {language}</p>
+            <div className="single-item__price">{price}$</div>
+        </SingleItemLayout>
     )
 }
 
