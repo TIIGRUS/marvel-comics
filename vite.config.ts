c46ba6d63a3df850ee089a6015a80d7ce0697e58
Владимир Шевченко <tiigrus@gmail.com>
2026-05-05 16:51:32 +0300

    fix: add missing css configuration in vite.config.ts for development sourcemaps

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), ViteImageOptimizer()],
+  css: {
+    devSourcemap: true,
+  }
 })
