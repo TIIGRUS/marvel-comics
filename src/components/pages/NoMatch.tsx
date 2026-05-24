a5c089257ffbe849343af045dbcab2917cfedc4c
Владимир Шевченко <tiigrus@gmail.com>
2026-05-24 18:12:14 +0300

    feat: implement 404 page with dedicated error styling and assets

--- src/components/pages/NoMatch.tsx
+++ src/components/pages/NoMatch.tsx
@@ -1,10 +1,20 @@
+import { Helmet } from "react-helmet-async";
 import ErrorMessage from "../ErrorMessage/ErrorMessage";
 
 const NoMatch = () => (
-  <ErrorMessage>
-    <h1>404 - Page Not Found</h1>
-    <p>Sorry, the page you are looking for does not exist.</p>
-  </ErrorMessage>
+  <>
+    <Helmet>
+      <meta
+        name="description"
+        content="Page not found - Marvel information portal"
+      />
+      <title>404 - Page Not Found</title>
+    </Helmet>
+    <ErrorMessage textLink="Go back to main page" pathLink="/">
+      <h1>404 - Page Not Found</h1>
+      <p>Sorry, the page you are looking for does not exist.</p>
+    </ErrorMessage>
+  </>
 );
 
 export default NoMatch;
